8dcc924832f9bb91d841f0b6ea66a3914ff8fe96
Y-Joo <wldudwn99@gmail.com>
2023-12-13 15:43:34 +0900

    Initial Commit

---
+++ src/main/java/com/example/yeobee/YeobeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.yeobee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeobeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YeobeeApplication.class, args);
+    }
+
+}
