b32cc2e7b131cbf4a1b1ebb77b2a1594028b53f2
Y-Joo <wldudwn99@gmail.com>
2023-12-28 14:21:26 +0900

    fix: swagger uri 변경

--- src/main/java/com/example/yeobee/common/swagger/SwaggerController.java
+++ src/main/java/com/example/yeobee/common/swagger/SwaggerController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/api/v1/usage")
+@RequestMapping("/v1/usage")
 class SwaggerController {
 
     @GetMapping
