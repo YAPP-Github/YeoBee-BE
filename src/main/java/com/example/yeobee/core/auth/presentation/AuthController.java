96e0516cee4c4d272fb4df3de7869d24235abf9f
Y-Joo <wldudwn99@gmail.com>
2023-12-30 15:39:33 +0900

    feat: 로그아웃 api 추가

--- src/main/java/com/example/yeobee/core/auth/presentation/AuthController.java
+++ src/main/java/com/example/yeobee/core/auth/presentation/AuthController.java
@@ -1,11 +1,13 @@
 package com.example.yeobee.core.auth.presentation;
 
+import com.example.yeobee.core.auth.annotation.AuthUser;
 import com.example.yeobee.core.auth.application.AppleAuthService;
 import com.example.yeobee.core.auth.application.AuthService;
 import com.example.yeobee.core.auth.application.KakaoAuthService;
 import com.example.yeobee.core.auth.dto.request.AppleLoginRequestDto;
 import com.example.yeobee.core.auth.dto.request.KakaoLoginRequestDto;
 import com.example.yeobee.core.auth.dto.response.TokenResponseDto;
+import com.example.yeobee.core.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -33,5 +35,11 @@ public class AuthController {
     @PostMapping(value = "/login/kakao")
     public ResponseEntity<TokenResponseDto> kakaoLogin(@RequestBody KakaoLoginRequestDto kakaoLoginRequest) {
         return ResponseEntity.ok(kakaoAuthService.login(kakaoLoginRequest));
+    }
+
+    @DeleteMapping(value = "/logout")
+    public ResponseEntity<Void> logout(@AuthUser User user){
+        authService.logout(user);
+        return ResponseEntity.noContent().build();
     }
 }
