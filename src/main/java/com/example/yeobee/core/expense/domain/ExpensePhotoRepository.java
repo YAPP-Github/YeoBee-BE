7cc41d5c839f8eacc27884a4003c7211bd98c50a
Y-Joo <wldudwn99@gmail.com>
2024-01-05 16:38:29 +0900

    feat: expensePhotoRepository 추가

---
+++ src/main/java/com/example/yeobee/core/expense/domain/ExpensePhotoRepository.java
@@ -0,0 +1,6 @@
+package com.example.yeobee.core.expense.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExpensePhotoRepository extends JpaRepository<ExpensePhoto, Long> {
+}
