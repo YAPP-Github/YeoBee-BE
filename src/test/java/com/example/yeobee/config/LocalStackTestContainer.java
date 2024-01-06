88b6a22a2b4176fe49d87c9fd51b4d726842ef04
gongmeda <gongmeda@gmail.com>
2024-01-06 17:20:58 +0900

    feat: S3용 LocalStack TestContatiner 적용

---
+++ src/test/java/com/example/yeobee/config/LocalStackTestContainer.java
@@ -0,0 +1,27 @@
+package com.example.yeobee.config;
+
+import static org.testcontainers.containers.localstack.LocalStackContainer.Service.S3;
+
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Component;
+import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@Component
+public class LocalStackTestContainer {
+
+    public static final String BUCKET_NAME = "yeobee-bucket";
+    private static final DockerImageName IMAGE_NAME = DockerImageName.parse("localstack/localstack");
+    public static final LocalStackContainer LOCAL_STACK_CONTAINER = new LocalStackContainer(IMAGE_NAME)
+        .withEnv("DEFAULT_REGION", "ap-northeast-2")
+        .withServices(S3);
+
+    static {
+        LOCAL_STACK_CONTAINER.start();
+    }
+
+    @PreDestroy
+    public void stop() {
+        LOCAL_STACK_CONTAINER.stop();
+    }
+}
