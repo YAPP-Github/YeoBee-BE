c53f05f54bd341bedcb3fce21fb8c5a80bfed7ce
Y-Joo <wldudwn99@gmail.com>
2024-01-24 17:13:57 +0900

    feat: tripUser 프로필 이미지 추가

--- src/main/java/com/example/yeobee/core/expense/dto/common/UserExpenseDetailDto.java
+++ src/main/java/com/example/yeobee/core/expense/dto/common/UserExpenseDetailDto.java
@@ -3,13 +3,16 @@ package com.example.yeobee.core.expense.dto.common;
 import com.example.yeobee.core.expense.domain.UserExpense;
 import java.math.BigDecimal;
 
-public record UserExpenseDetailDto(Long id, Long tripUserId, String tripUserName, BigDecimal amount) {
+public record UserExpenseDetailDto(Long id, Long tripUserId, String tripUserName, String profileImageUrl,
+                                   BigDecimal amount) {
 
     public UserExpenseDetailDto(UserExpense userExpense, Long userId) {
         this(userExpense.getId(),
              userExpense.getTripUser().getId(),
              !userId.equals(userExpense.getTripUser().getId()) ? userExpense.getTripUser().getName()
                  : userExpense.getTripUser().getName() + " (나)",
+             userExpense.getTripUser().getUser() == null ? null
+                 : userExpense.getTripUser().getUser().getProfileImageUrl(),
              userExpense.getAmount());
     }
 }
