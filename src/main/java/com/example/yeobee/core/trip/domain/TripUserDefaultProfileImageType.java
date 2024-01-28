a63de25c8f50756e85cc5bb806621966c15e41bf
Y-Joo <wldudwn99@gmail.com>
2024-01-28 13:30:41 +0900

    feat: 기본 이미지에 imageUrl 추가

--- src/main/java/com/example/yeobee/core/trip/domain/TripUserDefaultProfileImageType.java
+++ src/main/java/com/example/yeobee/core/trip/domain/TripUserDefaultProfileImageType.java
@@ -1,15 +1,22 @@
 package com.example.yeobee.core.trip.domain;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 // TODO: S3 이미지 경로 매핑 구현
+@Getter
+@RequiredArgsConstructor
 public enum TripUserDefaultProfileImageType {
-    IMAGE0,
-    IMAGE1,
-    IMAGE2,
-    IMAGE3,
-    IMAGE4,
-    IMAGE5,
-    IMAGE6,
-    IMAGE7,
-    IMAGE8,
-    IMAGE9,
+    IMAGE0("test0"),
+    IMAGE1("test1"),
+    IMAGE2("test2"),
+    IMAGE3("test3"),
+    IMAGE4("test4"),
+    IMAGE5("test5"),
+    IMAGE6("test6"),
+    IMAGE7("test7"),
+    IMAGE8("test8"),
+    IMAGE9("test9");
+    
+    private final String imageUrl;
 }
