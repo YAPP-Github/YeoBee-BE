16ce54e37c70af9819f37b34ac68a475bb3415cb
gongmeda <gongmeda@gmail.com>
2024-02-05 05:55:50 +0900

    feat: S3 이미지 초기화 구현

--- src/main/java/com/example/yeobee/common/config/InitRunner.java
+++ src/main/java/com/example/yeobee/common/config/InitRunner.java
@@ -10,7 +10,9 @@ import jakarta.persistence.Id;
 import java.io.BufferedReader;
 import java.lang.reflect.Field;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -21,21 +23,31 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 @Slf4j
 @RequiredArgsConstructor
 @Component
 public class InitRunner implements ApplicationRunner {
 
+    private static final String S3_FLAG_KEY_PATH = "static/country/flag/";
+
     private final EntityManager em;
+    private final S3Client s3Client;
 
     @Value("${yeobee.init:false}")
     private boolean runInit;
 
     @Value("${spring.cloud.aws.cdn.url}")
     private String cdnUrl;
+
+    @Value("${spring.cloud.aws.s3.bucket}")
+    private String bucket;
 
     @Transactional
     @Override
@@ -49,10 +61,45 @@ public class InitRunner implements ApplicationRunner {
         log.info("Start custom data initialization...");
         initS3();
         initDB();
+        log.info("Custom data initialization complete!");
     }
 
+    @SneakyThrows
     private void initS3() {
-        // TODO: 이미지 업로드 구현
+        String resourcePattern = "classpath*:init/s3/country/flag/**/*.*"; // Adjust path and pattern as needed
+        List<Resource> resources = listResources(resourcePattern);
+        for (Resource resource : resources) {
+            Path file = Path.of(resource.getURI());
+            uploadFileToS3(file);
+        }
+    }
+
+    @SneakyThrows
+    private List<Resource> listResources(String pattern) {
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        Resource[] resources = resolver.getResources(pattern);
+        List<Resource> fileList = new ArrayList<>();
+        for (Resource resource : resources) {
+            if (resource.isReadable()) {
+                fileList.add(resource);
+            }
+        }
+        return fileList;
+    }
+
+    @SneakyThrows
+    private void uploadFileToS3(Path filePath) {
+        String key = UrlUtil.join(S3_FLAG_KEY_PATH, filePath.getFileName().toString());
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+            .bucket(bucket)
+            .key(key)
+            .bucketKeyEnabled(true)
+            .build();
+
+        s3Client.putObject(putObjectRequest, filePath);
+
+        log.info("File uploaded successfully: {}", key);
     }
 
     @SneakyThrows
@@ -67,7 +114,7 @@ public class InitRunner implements ApplicationRunner {
         Field flagImageUrlField = Country.class.getDeclaredField("flagImageUrl");
         flagImageUrlField.setAccessible(true);
         for (Country country : countries) {
-            flagImageUrlField.set(country, UrlUtil.join(cdnUrl, "/static/country/flag/", country.getName() + ".svg"));
+            flagImageUrlField.set(country, UrlUtil.join(cdnUrl, S3_FLAG_KEY_PATH, country.getName() + ".svg"));
         }
         for (Country country : countries) {
             upsert(country);
