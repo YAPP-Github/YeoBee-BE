bea3535d0ac66012e9b3d376c0d521435601a604
Y-Joo <wldudwn99@gmail.com>
2024-02-08 01:09:23 +0900

    fix: controller 이름 수정

--- src/main/java/com/example/yeobee/core/calculation/presentation/TripCalculationController.java
+++ src/main/java/com/example/yeobee/core/calculation/presentation/TripCalculationController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/v1/trips")
 @RequiredArgsConstructor
-public class TripController {
+public class TripCalculationController {
 
     private final TripCalculationService tripCalculationService;
 
