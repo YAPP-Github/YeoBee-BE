1e4587a106a98e7c8c6b693152822a5890f7c035
Y-Joo <wldudwn99@gmail.com>
2024-02-12 17:27:38 +0900

    feat: 지출 상세 조회 dto 수정

--- src/main/java/com/example/yeobee/core/expense/dto/response/ExpenseDetailRetrieveResponseDto.java
+++ src/main/java/com/example/yeobee/core/expense/dto/response/ExpenseDetailRetrieveResponseDto.java
@@ -1,23 +1,27 @@
 package com.example.yeobee.core.expense.dto.response;
 
 import com.example.yeobee.core.expense.domain.Expense;
+import com.example.yeobee.core.expense.domain.ExpenseCategory;
 import com.example.yeobee.core.expense.domain.ExpenseMethod;
 import com.example.yeobee.core.expense.dto.ExpensePhotoDto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public record ExpenseDetailRetrieveResponseDto(BigDecimal amount, String currencyCode, Long koreanAmount,
-                                               ExpenseMethod expenseMethod, String expenseCategoryName,
-                                               String name, @Schema(nullable = true) Long payerUserId, String payerName,
+                                               LocalDateTime payedAt, ExpenseMethod expenseMethod,
+                                               ExpenseCategory expenseCategory, String name,
+                                               @Schema(nullable = true) Long payerUserId, String payerName,
                                                List<UserExpenseDetailDto> payerList, List<ExpensePhotoDto> imageList) {
 
     public ExpenseDetailRetrieveResponseDto(Expense expense) {
         this(expense.getAmount(),
              expense.getTripCurrency().getCurrency().getCode(),
              expense.getKoreanAmount(),
+             expense.getPayedAt(),
              expense.getExpenseMethod(),
-             expense.getExpenseCategory().getName(),
+             expense.getExpenseCategory(),
              expense.getName(),
              expense.getPayer().getUserId(),
              expense.getPayerName(),
