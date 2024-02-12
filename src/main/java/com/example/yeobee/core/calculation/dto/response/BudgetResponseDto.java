0b2db29de946b277cb9f4c3383ec9d174cb60d25
Y-Joo <wldudwn99@gmail.com>
2024-02-13 00:47:50 +0900

    fix 오타 수정

--- src/main/java/com/example/yeobee/core/calculation/dto/response/BudgetResponseDto.java
+++ src/main/java/com/example/yeobee/core/calculation/dto/response/BudgetResponseDto.java
@@ -2,7 +2,7 @@ package com.example.yeobee.core.calculation.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-public record BudgetResponseDto(Budget sharedBudget, Budget IndividualBudget) {
+public record BudgetResponseDto(Budget sharedBudget, Budget individualBudget) {
 
     public record Budget(@Schema(nullable = true) Long leftBudget, Long budgetIncome, Long budgetExpense) {
 
