6c04af13a417aaad98a1b416da496f30f77f3e6a
Y-Joo <wldudwn99@gmail.com>
2024-03-04 22:52:46 +0900

    feat: 지출 리스트 반환 dto 날짜 추가

--- src/main/java/com/example/yeobee/core/expense/dto/response/ExpenseListRetrieveResponseDto.java
+++ src/main/java/com/example/yeobee/core/expense/dto/response/ExpenseListRetrieveResponseDto.java
@@ -3,14 +3,17 @@ package com.example.yeobee.core.expense.dto.response;
 import com.example.yeobee.core.expense.domain.Expense;
 import com.example.yeobee.core.expense.domain.ExpenseCategory;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 public record ExpenseListRetrieveResponseDto(Long id, ExpenseCategory expenseCategoryImage, String name,
-                                             BigDecimal amount, String currencyCode, Long koreanAmount) {
+                                             LocalDateTime payedAt, BigDecimal amount, String currencyCode,
+                                             Long koreanAmount) {
 
     public ExpenseListRetrieveResponseDto(Expense expense) {
         this(expense.getId(),
              expense.getExpenseCategory(),
              expense.getName(),
+             expense.getPayedAt(),
              expense.getAmount(),
              expense.getCurrencyCode(),
              expense.getKoreanAmount());
--- src/main/java/com/example/yeobee/core/expense/dto/response/UserExpenseListRetrieveResponseDto.java
+++ src/main/java/com/example/yeobee/core/expense/dto/response/UserExpenseListRetrieveResponseDto.java
@@ -3,13 +3,16 @@ package com.example.yeobee.core.expense.dto.response;
 import com.example.yeobee.core.expense.domain.ExpenseCategory;
 import com.example.yeobee.core.expense.domain.UserExpense;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 public record UserExpenseListRetrieveResponseDto(ExpenseCategory expenseCategoryImage, String name,
-                                                 BigDecimal amount, String currencyCode, Long koreanAmount) {
+                                                 LocalDateTime payedAt, BigDecimal amount, String currencyCode,
+                                                 Long koreanAmount) {
 
     public UserExpenseListRetrieveResponseDto(UserExpense userExpense) {
         this(userExpense.getExpenseCategory(),
              userExpense.getExpenseName(),
+             userExpense.getExpense().getPayedAt(),
              userExpense.getAmount(),
              userExpense.getCurrencyCode(),
              userExpense.getKoreanAmount());
